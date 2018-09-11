efaaae6aba73632c05bd04dd7d85881149ecca0f
Marc S Delahousse <mdelahou@cisco.com>
2018-09-11 15:33:25 -0400

    ignore and setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='firexkit',
+      version='0.1',
+      description='Core firex libraries',
+      url='https://wwwin-github.cisco.com/firex/firexkit.git',
+      author='Core FireX Team',
+      author_email='firex-dev@gmail.com',
+      license='TBD',
+      packages=['firexkit', ],
+      zip_safe=False)
