30aada827aeb0e2232d22b9a1fe84596e7094e2b
Marc S Delahousse <mdelahou@cisco.com>
2018-11-14 12:09:16 -0500

    Argument converter for tasks. To be integrated into a FireXTask base class to convert pre and post microservice run

---
+++ firexkit/argument_convertion.py
@@ -0,0 +1,167 @@
+
+from collections import namedtuple
+from datetime import datetime
+from celery.utils.log import get_task_logger
+from celery.local import PromiseProxy
+
+from firexkit.bag_of_goodies import BagOfGoodies
+
+
+logger = get_task_logger(__name__)
+
+
+class ConverterRegister:
+    ConvertNode = namedtuple('ConvertNode', ['func', 'dependencies'])
+    _task_instances = {}
+
+    def __init__(self):
+        self._pre_converters = {}
+        self._post_converters = {}
+        self.visit_order = None
+
+    @classmethod
+    def task_convert(cls, task_name, pre_task=True, **kwargs) -> dict:
+        task_short_name = task_name.split('.')[-1]
+        if task_short_name not in cls._task_instances:
+            return kwargs
+        task = cls._task_instances[task_short_name]
+
+        return task.convert(pre_task=pre_task, **kwargs)
+
+    def convert(self, pre_task=True, **kwargs) -> dict:
+
+        # Recreate the kwargs and remove any argument string
+        # values which start with '@'.
+        new_kwargs = {}
+        for k, v in kwargs.items():
+            if isinstance(v, str):
+                if not v.startswith(BagOfGoodies.INDIRECT_ARG_CHAR):
+                    new_kwargs[k] = v
+            else:
+                new_kwargs[k] = v
+
+        # we converting pre or post
+        converters = self._pre_converters if pre_task else self._post_converters
+
+        for node in self.get_visit_order(pre_task):
+            start = datetime.now()
+            try:
+                converted_dict = converters[node].func(new_kwargs)
+            except Exception:
+                logger.error("Error in converter " + node)
+                raise
+            done = datetime.now()
+            logger.debug("Took %.3f seconds to convert %s" % ((done - start).total_seconds(), node))
+            # handle when None is returned
+            if converted_dict:
+                new_kwargs.update(converted_dict)
+        kwargs.update(new_kwargs)
+        return kwargs
+
+    def get_visit_order(self, pre_task=True):
+        converters = self._pre_converters if pre_task else self._post_converters
+        self.visit_order = []
+        for converter_node in converters.values():
+            self.visit_converter(converter_node=converter_node, all_converters=converters)
+        return self.visit_order
+
+    def visit_converter(self, all_converters, converter_node, depth=0):
+        if depth > len(all_converters):
+            raise CircularDependencyException("A circular dependency was detected between converters")
+
+        if converter_node.func.__name__ in self.visit_order:
+            return
+
+        for precursor in converter_node.dependencies:
+            if precursor not in all_converters and precursor not in self._pre_converters:
+                msg = precursor + " was not found. It is a dependency of " + converter_node.func.__name__
+                raise MissingConverterDependencyError(msg)
+
+            if converter_node.func.__name__ == precursor:
+                raise CircularDependencyException("A converter can not be dependant on itself")
+
+            if precursor not in self.visit_order and precursor in all_converters:
+                self.visit_converter(all_converters=all_converters,
+                                     converter_node=all_converters[precursor],
+                                     depth=depth+1)
+        self.visit_order.append(converter_node.func.__name__)
+
+    @classmethod
+    def register_for_task(cls, task: PromiseProxy, *args):
+        task_short_name = task.name.split('.')[-1]
+        if task_short_name not in cls._task_instances:
+            cls._task_instances[task_short_name] = ConverterRegister()
+        task_registry = cls._task_instances[task_short_name]
+        if not len(args):
+            raise Exception("Task Registration requires an additional bool parameter")
+        return task_registry.register(*args)
+
+    def register(self, *args):
+        """
+        Register a converter function. *arg is a collection of optional arguments, the function of which is based on
+        it's type:
+            callable (only once): A function that will be called to convert arguments
+            boolean (only once): At which point should this converter be called?
+                                False is pre (before task), True is post. (after task)
+            str: Dependencies. Any dependency of the current converter on the one in the string.
+        """
+        if len(args) is 0:
+            raise Exception("Registration requires at least one argument")
+
+        func = None
+        dependencies = []
+        run_post_task = None
+
+        for arg in args:
+            if callable(arg):
+                func = arg
+            elif isinstance(arg, str):
+                dependencies.append(arg)
+            elif isinstance(arg, bool):
+                run_post_task = arg
+            else:
+                raise Exception("Converter incorrectly registered")
+
+        return self._sub_register(func=func, dependencies=dependencies, run_post_task=run_post_task)
+
+    def _sub_register(self, func, dependencies: [], run_post_task):
+
+        if not run_post_task:
+            converters = self._pre_converters
+        else:
+            converters = self._post_converters
+
+        if func:
+            # this is the case where decorator is used WITHOUT parenthesis
+            # @InputConverter.register
+            self.check_not_registered(func)
+            converters[func.__name__] = self.ConvertNode(func=func, dependencies=[])
+            return func
+
+        # this is the case where decorator is used WITH parenthesis
+        # @ConverterRegister.register(...)
+        def _wrapped_register(fn):
+            self.check_not_registered(fn)
+            converters[fn.__name__] = self.ConvertNode(func=fn, dependencies=dependencies)
+            return fn
+        return _wrapped_register
+
+    def check_not_registered(self, func):
+        if callable(func):
+            func = func.__name__
+
+        if func in self._pre_converters or func in self._post_converters:
+            raise Exception("Converter %s is already registered. Please define a unique name" % func)
+
+    @classmethod
+    def get_register(cls, task_name):
+        task_short_name = task_name.split('.')[-1]
+        return cls._task_instances.get(task_short_name)
+
+
+class MissingConverterDependencyError(Exception):
+    pass
+
+
+class CircularDependencyException(Exception):
+    pass
---
+++ test/argument_conversion_tests.py
@@ -0,0 +1,151 @@
+
+import unittest
+from firexkit.argument_convertion import ConverterRegister, CircularDependencyException, MissingConverterDependencyError
+
+
+class ArgConversionTests(unittest.TestCase):
+
+    def test_converter_registration(self):
+        test_input_converter = ConverterRegister()
+
+        @test_input_converter.register
+        def converter_no_dependency(kwargs):
+            kwargs['converter_no_dependency'] = True
+            return kwargs
+
+        @test_input_converter.register('converter_no_dependency')
+        def converter_str_dependency(kwargs):
+            kwargs['converter_str_dependency'] = True
+            return kwargs
+
+        @test_input_converter.register('converter_no_dependency',
+                                       'converter_str_dependency')
+        def converter_list_dependency(kwargs):
+            kwargs['converter_list_dependency'] = True
+            return kwargs
+
+        converted = test_input_converter.convert(**{})
+        self.assertTrue('converter_no_dependency' in converted)
+        self.assertTrue('converter_str_dependency' in converted)
+        self.assertTrue('converter_list_dependency' in converted)
+
+        test_input_converter.pre_load_was_run = False
+        with self.assertRaises(MissingConverterDependencyError):
+            @test_input_converter.register('Nope')
+            def missing_dependent(kwargs):
+                return kwargs
+            test_input_converter.convert(**{})
+
+    def test_converter_dependency(self):
+        unit_test_obj = self
+
+        test_input_converter = ConverterRegister()
+
+        @test_input_converter.register
+        def converter_one(kwargs):
+            kwargs['converter_one'] = True
+            return kwargs
+
+        @test_input_converter.register('converter_one')
+        def converter_two(kwargs):
+            unit_test_obj.assertTrue('converter_one' in kwargs)
+            kwargs['converter_two'] = True
+            return kwargs
+
+        @test_input_converter.register('converter_four')
+        def converter_three(kwargs):
+            unit_test_obj.assertTrue('converter_four' in kwargs)
+            kwargs['converter_three'] = True
+            return kwargs
+
+        @test_input_converter.register
+        def converter_four(kwargs):
+            kwargs['converter_four'] = True
+            return kwargs
+
+        ############################
+        # test multiple dependencies
+        @test_input_converter.register('converter_one',
+                                       'converter_two',
+                                       'converter_three',
+                                       'converter_four')
+        def converter_five(kwargs):
+            unit_test_obj.assertTrue('converter_one' in kwargs)
+            unit_test_obj.assertTrue('converter_two' in kwargs)
+            unit_test_obj.assertTrue('converter_three' in kwargs)
+            unit_test_obj.assertTrue('converter_four' in kwargs)
+            return kwargs
+
+        test_input_converter.convert(**{})
+
+        #######################################
+        # test detection of circular dependency
+        test_input_converter = ConverterRegister()
+        with self.assertRaises(CircularDependencyException):
+            @test_input_converter.register('converter_seven')
+            def converter_six(kwargs):
+                kwargs['converter_six'] = True
+
+            @test_input_converter.register('converter_eight')
+            def converter_seven(kwargs):
+                kwargs['converter_seven'] = True
+
+            @test_input_converter.register('converter_six')
+            def converter_eight(kwargs):
+                kwargs['converter_eight'] = True
+            test_input_converter.convert(**{})
+
+        ################################
+        # test unrecognized dependencies
+        test_input_converter = ConverterRegister()
+        with self.assertRaises(MissingConverterDependencyError):
+            @test_input_converter.register("this_is_not_valid")
+            def converter_unrecognised(kwargs):
+                kwargs['converter_unrecognised'] = True
+            test_input_converter.convert(**{})
+
+        #####################################################
+        # test boolean to indicate after microservices loaded
+        test_input_converter = ConverterRegister()
+
+        @test_input_converter.register(True)
+        def converter_nine(kwargs):
+            kwargs['converter_nine'] = True
+
+        @test_input_converter.register(False)
+        def converter_ten(kwargs):
+            kwargs['converter_ten'] = True
+
+        @test_input_converter.register(False, "converter_ten")
+        def converter_eleven(kwargs):
+            kwargs['converter_eleven'] = True
+            unit_test_obj.assertTrue('converter_ten' in kwargs)
+
+        @test_input_converter.register("converter_eleven", False, "converter_ten")
+        def converter_twelve(kwargs):
+            unit_test_obj.assertTrue('converter_ten' in kwargs)
+            unit_test_obj.assertTrue('converter_eleven' in kwargs)
+
+        test_input_converter.convert(**{})
+
+        #####################################################
+        # test re cannot be dependant on post
+        test_input_converter = ConverterRegister()
+
+        @test_input_converter.register(False)
+        def converter_thirteen(kwargs):
+            kwargs['converter_thirteen'] = True
+
+        # post can be dependant on pre
+        @test_input_converter.register(True, "converter_thirteen")
+        def converter_fourteen(kwargs):
+            unit_test_obj.assertTrue('converter_thirteen' in kwargs)
+        kw = test_input_converter.convert(pre_task=True, **{})
+        test_input_converter.convert(pre_task=False, **kw)
+
+        @test_input_converter.register(False, "converter_fourteen")
+        def converter_fifteen(kwargs):
+            kwargs['converter_fifteen'] = True
+        with self.assertRaises(MissingConverterDependencyError):
+            kw = test_input_converter.convert(pre_task=True, **{})
+            test_input_converter.convert(pre_task=False, **kw)
