b48fc621ab02125840cb844d938f0324b8dc4656
Mohamed Abou El Saoud <mabouels@cisco.com>
2019-07-24 14:59:58 -0400

    Methods for app control inspection with retry capabilities for better resilience in the face of transient broker connectivity issues

---
+++ firexkit/inspect.py
@@ -0,0 +1,37 @@
+import time
+from celery import current_app
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+
+def inspect_with_retry(inspect_retry_timeout=30, inspect_method=None, **inspect_opts):
+    def _inspect():
+        i = current_app.control.inspect(**inspect_opts)
+        if inspect_method:
+            return getattr(i, inspect_method)()
+        else:
+            return i
+
+    if inspect_retry_timeout:
+        timeout_time = time.time() + inspect_retry_timeout
+        tries = 0
+        while time.time() < timeout_time:
+            tries += 1
+            try:
+                return _inspect()
+            except Exception as e:
+                logger.debug(e)
+                logger.debug('Inspection failed. Retrying for up to %r seconds' % inspect_retry_timeout)
+                time.sleep(0.1)
+    return _inspect()
+
+
+def get_active_tasks(**kwargs):
+    kwargs.pop('inspect_method', None)
+    return inspect_with_retry(inspect_method='active', **kwargs)
+
+
+def get_revoked_tasks(**kwargs):
+    kwargs.pop('inspect_method', None)
+    return inspect_with_retry(inspect_method='revoked', **kwargs)
