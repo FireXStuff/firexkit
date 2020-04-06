b7a31676504936a2595185e9454bc7691d233274
Mohamed Abou El Saoud <mabouels@cisco.com>
2020-04-06 19:54:03 -0400

    Fix bug causing a raise before retry. Temporarily instrument code to send-event when retrying

--- firexkit/broker.py
+++ firexkit/broker.py
@@ -6,9 +6,10 @@ logger = get_task_logger(__name__)
 
 def handle_broker_timeout(callable_func, args=(), kwargs={}, timeout=None, retry_delay=0.1):
     timeout_time = time.time() + timeout if timeout else None
+    retries = 0
     while True:
         try:
-            return callable_func(*args, **kwargs)
+            return_value = callable_func(*args, **kwargs)
         except Exception as e:
             # need to handle different timeout exceptions from different brokers
             if type(e).__name__ != "TimeoutError":
@@ -17,11 +18,33 @@ def handle_broker_timeout(callable_func, args=(), kwargs={}, timeout=None, retry
                 logger.warning(f'Backend was not reachable and timed out...'
                                f'retrying in {retry_delay}s')
                 time.sleep(retry_delay)
+                retries += 1
             else:
-                if time.time() < timeout_time:
+                current_time = time.time()
+                if current_time < timeout_time:
                     logger.warning(f'Backend was not reachable and timed out...'
-                                   f'retrying in {retry_delay}s for a max of {timeout}s')
+                                   f'retrying in {retry_delay}s for a max of {timeout}s ({timeout_time-current_time:.2f}s remaining)')
                     time.sleep(retry_delay)
+                    retries += 1
                 else:
                     logger.error(f'Reached max timeout of {timeout}...giving up!')
-                raise
+                    try:
+                        instrument_and_send_event('handle_broker_timeout-failure', broker_retries=retries)
+                    finally:
+                        raise
+        else:
+            try:
+                instrument_and_send_event('handle_broker_timeout-success', broker_retries=retries)
+            finally:
+                return return_value
+
+
+def instrument_and_send_event(instrumentation_label, **kwargs):
+    from celery import current_task
+    if not current_task:
+        return
+    try:
+        current_task.send_event('task-instrumentation', instrumentation_label=instrumentation_label, **kwargs)
+    except Exception as e:
+        logger.debug(e, exc_info=True)
+        logger.debug('Could not instrument')
