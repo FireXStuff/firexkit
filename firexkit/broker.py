3411f1aa5bdf69cff9c86a736fb16b7d4ff8739f
Mohamed Abou El Saoud <mabouels@cisco.com>
2020-04-06 22:50:45 -0400

    Include call stack in instrumentation

--- firexkit/broker.py
+++ firexkit/broker.py
@@ -1,13 +1,13 @@
 import time
 from celery.utils.log import get_task_logger
-
 logger = get_task_logger(__name__)
 
 
 def handle_broker_timeout(callable_func, args=(), kwargs={}, timeout=None, retry_delay=0.1):
     timeout_time = time.time() + timeout if timeout else None
-    retries = 0
+    tries = 0
     while True:
+        tries += 1
         try:
             return_value = callable_func(*args, **kwargs)
         except Exception as e:
@@ -18,33 +18,36 @@ def handle_broker_timeout(callable_func, args=(), kwargs={}, timeout=None, retry
                 logger.warning(f'Backend was not reachable and timed out...'
                                f'retrying in {retry_delay}s')
                 time.sleep(retry_delay)
-                retries += 1
             else:
                 current_time = time.time()
                 if current_time < timeout_time:
                     logger.warning(f'Backend was not reachable and timed out...'
-                                   f'retrying in {retry_delay}s for a max of {timeout}s ({timeout_time-current_time:.2f}s remaining)')
+                                   f'retrying in {retry_delay}s for a max of {timeout}s '
+                                   f'({timeout_time-current_time:.2f}s remaining)')
                     time.sleep(retry_delay)
-                    retries += 1
                 else:
-                    logger.error(f'Reached max timeout of {timeout}...giving up!')
+                    logger.error(f'Reached max timeout of {timeout}s...giving up!')
                     try:
-                        instrument_and_send_event('handle_broker_timeout-failure', broker_retries=retries)
+                        send_task_instrumentation_event(event_label='handle_broker_timeout-failure',
+                                                        broker_timeout_tries=tries)
                     finally:
                         raise
         else:
             try:
-                instrument_and_send_event('handle_broker_timeout-success', broker_retries=retries)
+                if tries > 1:
+                    send_task_instrumentation_event(event_label='handle_broker_timeout-success',
+                                                    broker_timeout_tries=tries)
             finally:
                 return return_value
 
 
-def instrument_and_send_event(instrumentation_label, **kwargs):
+def send_task_instrumentation_event(event_type='task-instrumentation', **fields):
     from celery import current_task
     if not current_task:
         return
     try:
-        current_task.send_event('task-instrumentation', instrumentation_label=instrumentation_label, **kwargs)
+        import traceback
+        current_task.send_event(event_type, instrumentation_event_stack=traceback.format_stack(), **fields)
     except Exception as e:
         logger.debug(e, exc_info=True)
         logger.debug('Could not instrument')
