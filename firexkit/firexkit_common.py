0c06b7ccc6c438e60d3c01d174b2dde464d9026a
Mohamed Abou El Saoud <mabouels@cisco.com>
2021-02-19 17:49:54 -0500

    Better naming

--- firexkit/firexkit_common.py
+++ firexkit/firexkit_common.py
@@ -23,7 +23,7 @@ def get_link(url, text=None, html_class=None, title_attribute=None, attrs=None, 
     )
 
 
-class ChangeUmask(object):
+class ModifyUmask(object):
     def __init__(self, new_umask):
         self.new_umask = new_umask
 
