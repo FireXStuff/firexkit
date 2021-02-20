534d951eee59db34e3b37bcfe685967443853d30
Mohamed Abou El Saoud <mabouels@cisco.com>
2021-02-19 21:30:13 -0500

    Add some more commons UMASKs and CHMOD modes

--- firexkit/permissions.py
+++ firexkit/permissions.py
@@ -1,9 +1,11 @@
-# ug=rwx
+# ug=rwx,o=
 # This ensures the last three bits are cleared, but, doesn't set any bits.
 DEFAULT_UMASK = 0o0007
-
-# ug+rwx
 DEFAULT_CHMOD_MODE = 0o770
 
 # ug+rw
-CHMOD_MODE_NONEXEC = 0o660
+NONEXEC_CHMOD_MODE = 0o660
+
+#u=rwx,g=rx,o=
+NO_GROUP_WRITE_UMASK = 0o0027
+NO_GROUP_WRITE_CHMOD_MODE = 0o750
