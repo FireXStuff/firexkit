edffde6f96ccb6ded025797cb6736b62c1d82d50
Mohamed Abou El Saoud <mabouels@cisco.com>
2021-02-22 13:15:53 -0500

    Open permissions to facilitate docker deployments

--- firexkit/permissions.py
+++ firexkit/permissions.py
@@ -1,11 +1,3 @@
-# ug=rwx,o=
-# This ensures the last three bits are cleared, but, doesn't set any bits.
-DEFAULT_UMASK = 0o0007
-DEFAULT_CHMOD_MODE = 0o770
-
-# ug+rw
-NONEXEC_CHMOD_MODE = 0o660
-
-#u=rwx,g=rx,o=
-NO_GROUP_WRITE_UMASK = 0o0027
-NO_GROUP_WRITE_CHMOD_MODE = 0o750
+# ugo=rw,o=rx
+DEFAULT_UMASK = 0o0002
+DEFAULT_CHMOD_MODE = 0o775
